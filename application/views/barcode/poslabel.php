5136dd1d34e98329610d908718e25a801a46f1e4
Sufyan Naveed <sufyannaveed28@gmail.com>
2023-01-12 18:27:12 +0500

    barcode price increase

--- application/views/barcode/poslabel.php
+++ application/views/barcode/poslabel.php
@@ -8,16 +8,11 @@
             sheet-size: 250px 50mm;
 
         }
-
         @media print
         {
             h1{
                 font-size:250px !important; 
             }
-            span{
-                font-size:250px !important; 
-            }
-
             p{
                 font-size:88px !important; 
             }
@@ -29,17 +24,19 @@
 <table cellpadding="10" style="width: 100%;">
     <tr>
         <td>
-            <h1 style="bordfont font-size:250px;"><?= $lab['product_name'] ?></h1>
+            <h1 style="bordfont font-size:200px;"><?= $lab['product_name'] ?></h1>
             <br><br><br><br><br><br><br><br>
-            <span style="bordfont font-size:250px;"><strong><?= $lab['product_code'] ?></span></strong>
+            <span style="bordfont font-size:200px;"><strong><?= $lab['product_code'] ?></span></strong>
             
             <br>
             <br>
             <barcode code="<?= $lab['barcode'] ?>" text="0" class="barcode" width="2" size="25" height=".3"/>
             </barcode>
             <br><br>
-            <h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <?= amountExchange($lab['product_price'], 0, $this->aauth->get_user()->loc) ?> </strong></h1>
+            <span style="bordfont font-size:350px;">
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <strong> <?= amountExchange($lab['product_price'], 0, $this->aauth->get_user()->loc) ?> </strong>
+            </span>
         </td>
     </tr>
 </table>
